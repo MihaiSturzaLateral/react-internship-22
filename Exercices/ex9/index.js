042cd5afe616c16bf64d5935b51a08e90638d14d
Ionut Iova <iova_ionut99@yahoo.com>
2022-07-26 01:04:12 +0300

    exercitiu 9

---
+++ Exercices/ex9/index.js
@@ -0,0 +1,31 @@
+//exercise 9
+
+let api = 'https://jsonplaceholder.typicode.com/photos';
+const photoResult = document.getElementById('photoResult');
+const btn = document.getElementById('photoBtn');
+
+//with THEN
+/*
+const getRandomPhoto = () => {
+    fetch(api)
+        .then(res => res.json())
+        .then(data => {
+            let random = Math.floor(Math.random()*5000);
+            photoResult.innerHTML += `<div class="result"><img src="${data[random].url}"/></div>`
+        })
+}
+btn.addEventListener('click', getRandomPhoto)
+*/
+
+//with ASYNC, AWAIT
+
+const randomPhoto = async() => {
+    const res = await fetch(api);
+    const data = await res.json();
+    let random1 = Math.floor(Math.random()*5000);
+    photoResult.innerHTML += `<div class="result"><img src="${data[random1].url}"/></div>`
+
+
+}
+
+btn.addEventListener("click", randomPhoto);
