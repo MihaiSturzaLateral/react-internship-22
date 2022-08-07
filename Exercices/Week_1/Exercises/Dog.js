f3585499629bda8938a8807f7433c6eecb4ff64d
adelatanca99 <95678533+adelatanca99@users.noreply.github.com>
2022-08-07 21:43:47 +0300

    Update Dog.js

--- Exercices/Week_1/Exercises/Dog.js
+++ Exercices/Week_1/Exercises/Dog.js
@@ -3,10 +3,12 @@ const { Animal } = require('./Animal.js')
 
 class Dog extends Animal {
     constructor(breed, color, name, legs) {
+        --> super method first
+        super(color,name,legs);
         this.breed = breed
-        super(color)
-        super(name)
-        super(legs)
+        super(color) //wrong
+        super(name) //wrong
+        super(legs) //wrong
     }
 
     show() {
