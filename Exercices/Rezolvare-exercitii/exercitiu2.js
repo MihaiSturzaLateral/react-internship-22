a06f0980b3a7b11acf438954568a9e0d6e9d3c85
Adela Tanca <adela.tanca@lateral-inc.com>
2022-08-07 22:12:21 +0300

    observations

--- Exercices/Rezolvare-exercitii/exercitiu2.js
+++ Exercices/Rezolvare-exercitii/exercitiu2.js
@@ -1,7 +1,7 @@
 //2. Create an array with countries and find the countries that contain only 5 characters.
 // (Print them to the console) Spain, China, India, Japan…
 const tari = ["Spain", "Romania", "France", "Japan", "China", "India"];
-console.log(tari.filter((tara) => tara.length < 6));
+console.log(tari.filter((tara) => tara.length < 6)); // the condition could be better, what if you have countries with 4 characters? Always pay attention to the filter condition
 
 /* ----- SAU mai putem face asa: -----
 
