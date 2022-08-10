a3a24a88f95b2c592b2d8e45b1eca3aabaae66a5
[andreea sidea] <sidea.andreeageorgiana@gmail.com>
2022-08-10 14:48:33 +0300

    clean-up

--- Exercices/Week1/exercitiul2.js
+++
@@ -1,9 +0,0 @@
-/*2. Create an array with countries and find the countries that contain only 5 characters.
-(Print them to the console) Spain, China, India, Japan…*/
-
-const countries = ['Spain', 'China', 'India', 'Japan', 'Germania']
-for(let i = 0; i < countries.length; i++){
-    if(countries[i].length==5){
-        console.log(countries[i]);
-    }
-}
