f6ecf9b7a6ec070648492fe36982ebb4a1277071
Adela Tanca <adela.tanca@lateral-inc.com>
2022-08-12 10:46:41 +0300

    added formik boilerplate

---
+++ Exercices/formik/src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './App.css'
+import Form from './components/Form';
+
+const App = () => {
+  return (
+    <>
+      <div className='container'>Forms with Formik</div>
+      <div className="App">
+        <Form />
+      </div>
+    </>
+  );
+};
+
+export default App;
